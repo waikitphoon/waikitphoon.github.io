61b922aaa63eafd6e59e5c501fca8469fe9aaa19
wkitphoon <wphoon@moneylion.com>
2022-07-09 10:50:27 +0800

    dev

---
+++ src/containers/experienceAccordion/ExperienceAccordion.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ExperienceCard from "../../components/experienceCard/ExperienceCard.js";
+import "./ExperienceAccordion.css";
+import { Accordion, Panel } from "baseui/accordion";
+import { DarkTheme, LightTheme, ThemeProvider } from "baseui";
+
+function ExperienceAccordion(props) {
+  const theme = props.theme;
+
+  return (
+    <div className="experience-accord">
+      <ThemeProvider theme={theme.name === "light" ? LightTheme : DarkTheme}>
+        <Accordion>
+          {props.sections.map((section) => {
+            return (
+              <Panel
+                expanded="true"
+                className="accord-panel"
+                title={section["title"]}
+                key={section["title"]}
+              >
+                {section["experiences"].map((experience) => {
+                  return (
+                    <ExperienceCard experience={experience} theme={theme} />
+                  );
+                })}
+              </Panel>
+            );
+          })}
+        </Accordion>
+      </ThemeProvider>
+    </div>
+  );
+}
+
+export default ExperienceAccordion;
