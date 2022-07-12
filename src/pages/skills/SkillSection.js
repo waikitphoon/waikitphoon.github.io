ae789ed0842e63261d6e535cc24fe16ce6885dd1
wkitphoon <wphoon@moneylion.com>
2022-07-12 22:33:55 +0800

    fix headers

--- src/pages/skills/SkillSection.js
+++ src/pages/skills/SkillSection.js
@@ -3,17 +3,6 @@ import "./Skills.css";
 import SoftwareSkill from "../../components/softwareSkills/SoftwareSkill";
 import { skills } from "../../portfolio";
 import { Fade } from "react-reveal";
-import FullStackImg from "./FullStackImg";
-import CloudInfraImg from "./CloudInfraImg";
-// import DesignImg from "./DesignImg";
-
-function GetSkillSvg(props) {
-  if (props.fileName === "FullStackImg")
-    return <FullStackImg theme={props.theme} />;
-  else if (props.fileName === "CloudInfraImg")
-    return <CloudInfraImg theme={props.theme} />;
-  // return <DesignImg theme={props.theme} />;
-}
 
 function SkillSection(props) {
   const theme = props.theme;
@@ -22,12 +11,6 @@ function SkillSection(props) {
       {skills.data.map((skill, index) => {
         return (
           <div className="skills-main-div">
-            {/* <Fade left duration={2000}>
-                <div className="skills-image-div">
-                  <GetSkillSvg fileName={skill.fileName} theme={theme} />
-                </div>
-              </Fade> */}
-
             <div className="skills-text-div">
               <Fade right duration={1000}>
                 <h1 className="skills-heading" style={{ color: theme.text }}>
